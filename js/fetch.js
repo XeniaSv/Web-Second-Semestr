22c68867d3ef68dfd195e4c4518a7aa11d6511c2
Xenia <savelicheva123@gmail.com>
2021-04-09 15:22:09 +0300

    Correct for heroku

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,4 @@
-const serverURL = "http://localhost:3000";
+const serverURL = "https://cryptic-tundra-69345.herokuapp.com";
 const weatherURL = `${serverURL}/weather/city`;
 const weatherCoordinatesURL = `${serverURL}/weather/coordinates`;
 const favouritesURL = `${serverURL}/favourites`;
